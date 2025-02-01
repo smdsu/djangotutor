d568bbd7d8ee4bf7ad9c33c023084f790297ade7
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-01 11:02:29 +0300

    Add a Similar posts
    Added recommendation of similar posts by number of matching tags

--- mysite/my_blog/views.py
+++ mysite/my_blog/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.db.models import Count
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from .models import Post
@@ -48,6 +48,14 @@ def post_detail(request, year, month, day, post):
 
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     return render(
         request,
         "blog/post/detail.html",
@@ -55,6 +63,7 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'form': form,
+            'similar_posts': similar_posts,
         },
     )
 
