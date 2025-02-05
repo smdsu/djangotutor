abdc9acb5bafef2f8703265057d5ed10c7b55f41
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-05 15:34:23 +0300

    Add a RSS Feed

--- mysite/my_blog/urls.py
+++ mysite/my_blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from . import views
+from .feeds import LatestPostFeed
 
 
 app_name = 'blog'
@@ -9,10 +11,11 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     #path('', views.PostListView.as_view(), name='post_list'),
     path(
-        '<int:year>/<int:month>/<int:day>/<slug:post>',
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
         name='post_detail'
     ),
     path("<int:post_id>/share/", views.post_share, name=('post_share')),
-    path("<int:post_id>/comment", views.post_comment, name=('post_comment')),
+    path("<int:post_id>/comment/", views.post_comment, name=('post_comment')),
+    path("feed/", LatestPostFeed(), name='post_feed')
 ]
