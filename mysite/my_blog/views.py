ce13135b34acf73f020b970a6d78d6e55946182e
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-12 19:03:02 +0300

    Create search view and refactor imports

--- mysite/my_blog/views.py
+++ mysite/my_blog/views.py
@@ -1,11 +1,17 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.core.mail import send_mail
+
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
+
 from django.views.decorators.http import require_POST
+
+from django.contrib.postgres.search import SearchVector
+
 from taggit.models import Tag
+
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 def post_list(request, tag_slug=None):
@@ -134,5 +140,30 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment,
+        }
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body')
+                ).filter(search=query)
+            )
+    
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results,
         }
     )
