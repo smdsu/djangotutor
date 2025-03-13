d86cc989e5cc6d9b4a341c30f9175c40d154bb67
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-13 14:43:27 +0300

    Add search ranking

--- mysite/my_blog/views.py
+++ mysite/my_blog/views.py
@@ -6,7 +6,11 @@ from django.db.models import Count
 
 from django.views.decorators.http import require_POST
 
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
 
 from taggit.models import Tag
 
@@ -152,10 +156,19 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector(
+                'title', weight='A'
+                ) + SearchVector(
+                    'body', weight='B'
+                )
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body')
-                ).filter(search=query)
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                )
+                .filter(rank__gte=0.3)
+                .order_by('-rank')
             )
     
     return render(
