da30f8079e80051aec4f8dc4f006351781bc196a
smdsu <109036946+smdsu@users.noreply.github.com>
2025-03-13 14:46:31 +0300

    Add Trigram Similarity

--- mysite/my_blog/views.py
+++ mysite/my_blog/views.py
@@ -6,11 +6,7 @@ from django.db.models import Count
 
 from django.views.decorators.http import require_POST
 
-from django.contrib.postgres.search import (
-    SearchVector,
-    SearchQuery,
-    SearchRank,
-)
+from django.contrib.postgres.search import TrigramSimilarity
 
 from taggit.models import Tag
 
@@ -156,19 +152,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector(
-                'title', weight='A'
-                ) + SearchVector(
-                    'body', weight='B'
-                )
-            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query)
+                    similarity=TrigramSimilarity('title', query),
                 )
-                .filter(rank__gte=0.3)
-                .order_by('-rank')
+                .filter(similarity__gt=0.1)
+                .order_by('-similarity')
             )
     
     return render(
